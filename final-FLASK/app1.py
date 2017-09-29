d39689fbb216427d58087868cc4c8b9ef8c0708f
Henzel Santos <henzelmsn3002@hotmail.com>
2017-09-29 09:12:45 -0300

    Add files via upload
    Just a backup from my website created with flask.

---
+++ final-FLASK/app1.py
@@ -0,0 +1,125 @@
+#UTF-8
+import sys
+from flask import Flask, render_template, request, url_for, redirect, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import Form
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import DataRequired
+from flask_login import LoginManager, login_user
+
+
+app = Flask(__name__)
+
+app.config.from_object('config')
+
+
+db = SQLAlchemy(app)
+
+
+lm = LoginManager()
+lm.init_app(app)
+
+
+@lm.user_loader
+def load_user(id):
+    return User.get(id)
+
+
+import sys
+# sys.setdefaultencoding() does not exist, here!
+reload(sys)  # Reload does the trick!
+sys.setdefaultencoding('UTF8')
+
+class LoginForm(Form):
+    openid = StringField('openid', validators=[DataRequired()])
+    remember_me = BooleanField('remember_me', default=False)
+    email = StringField("email", validators=[DataRequired()])
+    senha = PasswordField("senha", validators=[DataRequired()])
+    
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    senha = db.Column(db.String(16))
+    email = db.Column(db.String(50), unique=True)
+
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return unicode(self.id)
+
+
+    def __init__(self,senha,email):
+        self.senha = senha
+        self.email = email
+
+db.create_all()
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return render_template("index.html")
+
+# user = User.query.filter_by(email=form.emai.data).first()
+ #        if user and user.senha == form.senha.data:
+  #          login_user(user)
+   #         flash("logado com sucesso")
+    #    else:
+     #       flash("login invalido")
+
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    user = User.query.filter_by(email=form.email.data).first()
+    if user is not None:
+        if user.senha == form.senha.data:
+            flash("Usuario logado com sucesso")
+            return redirect(url_for("logged"))
+        else :
+            flash("login invalido")
+            return redirect(url_for("index"))
+    return render_template("login.html",form=form)
+
+
+@app.route("/signup", methods=['GET', 'POST'])
+def signup():
+    form = LoginForm()
+    if request.method == 'POST':
+        senha = (request.form.get("senha"))
+        email = (request.form.get("email"))
+        new_user = User(senha=senha,email=email)
+        
+        db.session.add(new_user)
+        db.session.commit()
+        flash("Usuario Cadastrado com Sucesso")
+    return render_template("signup.html",form=form)
+
+
+
+@app.route("/logged")
+def logged():
+    return render_template("logged.html")
+
+
+@app.route("/contact")
+def contato():
+    return render_template("contact.html")
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ final-FLASK/config.py
@@ -0,0 +1,15 @@
+DEBUG = True
+
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:boladebola@localhost/ultima_vez'
+
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+		
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'you-will-never-guess'
+
+OPENID_PROVIDERS = [
+    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
+    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
+    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
